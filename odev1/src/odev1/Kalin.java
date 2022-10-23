9b40bbd7b3ea8182acacd14d2d15dbad322ac7f5
Ensar Guven <ensarguvenofficial@gmail.com>
2022-10-23 12:55:39 +0300

    First commit.

---
+++ odev1/src/odev1/Kalin.java
@@ -0,0 +1,32 @@
+package odev1;
+
+public class Kalin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+	        char haf ='E';
+			char[] JavaCharArray = new char[5];
+			JavaCharArray[0] = 'A';
+			JavaCharArray[1] = 'O';
+			JavaCharArray[2] = 'U';
+			JavaCharArray[3] = 'E';
+			
+			
+			for (int i = 0; i < JavaCharArray.length; i++) {
+				if(haf  == JavaCharArray[i]) {
+				System.out.println("KALIN");
+				 return;
+				}
+				
+				
+			
+					
+			}
+			
+			System.out.println("ince");
+
+	}
+
+}
